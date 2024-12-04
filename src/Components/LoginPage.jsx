08b6558d11cab2f5c122b5d4675feba07c2c20ff
sujay <sujayb255@gmail.com>
2024-12-03 21:52:33 -0600

    ui changrs

--- src/Components/LoginPage.jsx
+++ src/Components/LoginPage.jsx
@@ -24,7 +24,7 @@ function LoginPage({ setLoggedIn, setUserData }) {
     try {
       const userData = await loginUser({ email, password });
       localStorage.setItem("loggedIn", "true");
-      userData.role = "admin";
+    
       localStorage.setItem("userDetails", JSON.stringify(userData));
       console.log(userData);
       setUserData(userData);
