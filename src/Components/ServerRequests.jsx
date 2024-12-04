1af6923f28529a95972c1f60ae347c4e44231992
srinivastherapati <vasu462462@gmail.com>
2024-12-03 21:52:33 -0600

    ui changrs

--- src/Components/ServerRequests.jsx
+++ src/Components/ServerRequests.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL = "/api";
+export const API_BASE_URL = "http://localhost:8080/api";
 // Register user
 export const registerUser = async (userData) => {
   try {
