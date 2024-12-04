17fde122f6d30e4148343ecb185c67b05eee1c90
Sujay Bashetty <sujay@Sujays-MacBook.local>
2024-12-04 00:57:16 -0500

    changed orders page

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://rich-lies-dream.loca.lt/api",
+        target: "https://chubby-vans-relate.loca.lt/api",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
