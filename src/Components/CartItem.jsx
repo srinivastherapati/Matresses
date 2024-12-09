69c3d724474a50d0371f1da21d0b9ee1f25e1915
Sujay Bashetty <sujay@Sujays-MacBook.local>
2024-12-09 14:23:16 -0500

    udpated code

--- src/Components/CartItem.jsx
+++ src/Components/CartItem.jsx
@@ -1,8 +1,8 @@
 
-export default function CartItem({name,quantity,price,onIncrease,onDecrease}){
+export default function CartItem({name,quantity,price,onIncrease,onDecrease, size, dimension}){
     return( <li className="cart-item">
         <p>
-            {name }- {quantity}  X {price}
+            {name }- {quantity}  X {price}- {size} - {dimension}
         </p>
         <p className="cart-item-actions">
             <button onClick={onDecrease}>-</button>
